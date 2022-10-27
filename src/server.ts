d4db350e6ad62630f4debc3e7628027b2eac1130
Demir Boraj <demirboraj97@gmail.com>
2022-10-27 14:56:12 +0200

    1

--- src/server.ts
+++ src/server.ts
@@ -366,22 +366,8 @@ app.post("/cartItem", async (req, res) => {
 
 app.get("/cartItems", async (req, res) => {
   try {
-<<<<<<< HEAD
-    const token = req.headers.authorization;
-    if (!token) {
-      res.status(404).send({ errors: ["Token not found"] });
-      return;
-    }
-    const user = await getCurrentUser(token);
-    if (!user) {
-      res.status(404).send({ errors: ["Invalid token"] });
-      return;
-    }
-    res.send(user.cart);
-=======
     const cart = prisma.cartItem.findMany();
     res.send(cart);
->>>>>>> d53ea9876f0651ccf4767e1f2f9d3cb18ffc7cb4
   } catch (error) {
     //@ts-ignore
     res.status(400).send({ errors: [error.message] });
@@ -420,12 +406,8 @@ app.delete("/cartItem/:id", async (req, res) => {
       res.status(404).send({ errors: ["Cart item not found"] });
       return;
     }
-<<<<<<< HEAD
-    res.send(user.cart);
-=======
 
     res.send(user);
->>>>>>> d53ea9876f0651ccf4767e1f2f9d3cb18ffc7cb4
   } catch (error) {
     //@ts-ignore
     res.status(400).send({ errors: [error.message] });
@@ -433,56 +415,12 @@ app.delete("/cartItem/:id", async (req, res) => {
 });
 app.patch("/changeUserBalance/:id", async (req, res) => {
   try {
-<<<<<<< HEAD
-    const token = req.headers.authorization;
-    if (token) {
-      const user = await getCurrentUser(token);
-      if (!user) {
-        res.status(400).send({ errors: ["Invalid token"] });
-      } else {
-        //2. Calculate the total from the cart
-        let total = 0;
-
-        for (let item of user.cart) {
-          total += item.course.price;
-        }
-
-        //3. If the user has enough balance buy every course
-        if (total < user.balance) {
-          //4. Create a boughtCourse and delete the cartItem for each course in the cart
-
-          for (let item of user.cart) {
-            await prisma.boughtCourse.create({
-              data: {
-                userId: item.userId,
-                courseId: item.courseId,
-              },
-            });
-
-            await prisma.cartItem.delete({ where: { id: item.id } });
-          }
-          await prisma.user.update({
-            where: { id: user.id },
-            data: {
-              balance: user.balance - total,
-            },
-          });
-          res.send({ message: "Order successful!" });
-        } else {
-          res.status(400).send({ errors: ["You're broke"] });
-        }
-      }
-    } else {
-      res.status(400).send({ errors: ["Token not found"] });
-    }
-=======
     const id = Number(req.params.id);
     const user = await prisma.user.findUnique({
       where: { id: id },
       select: { balance: req.body.balance },
     });
     res.send(user);
->>>>>>> d53ea9876f0651ccf4767e1f2f9d3cb18ffc7cb4
   } catch (error) {
     //@ts-ignore
     res.status(400).send({ errors: [error.message] });
